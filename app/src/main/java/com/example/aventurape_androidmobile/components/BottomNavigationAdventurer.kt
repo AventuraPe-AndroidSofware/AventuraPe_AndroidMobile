61485149b11a94e902d576d420db8e69d7553a1c
aksonie <u202210778@upc.edu.pe>
2024-09-17 20:46:42 -0500

    feat: add fuctional navigation bar colors

--- app/src/main/java/com/example/aventurape_androidmobile/components/BottomNavigationAdventurer.kt
+++ app/src/main/java/com/example/aventurape_androidmobile/components/BottomNavigationAdventurer.kt
@@ -1,11 +1,18 @@
 package com.example.aventurape_androidmobile.components
 
+import androidx.compose.foundation.background
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.aventurape_androidmobile.models.Items_bottom_navA.*
 import com.example.aventurape_androidmobile.navigation.currentRoute
@@ -14,24 +21,29 @@ import com.example.aventurape_androidmobile.navigation.currentRoute
 fun BottomNavigationAdventurer(
     navController: NavHostController
 ) {
-    val menu_items= listOf(
+    val menu_items = listOf(
         Item_bottom_nav1,
         Item_bottom_nav2,
         Item_bottom_nav3
     )
     BottomAppBar {
-        NavigationBar (
-            containerColor = MaterialTheme.colorScheme.inverseSurface
-        ){
-            menu_items.forEach{ item ->
-                val selected = currentRoute(navController)==item.ruta
+        NavigationBar(
+            containerColor = Color.Transparent
+        ) {
+            menu_items.forEach { item ->
+                val selected = currentRoute(navController) == item.ruta
                 NavigationBarItem(
                     selected = selected,
                     onClick = { navController.navigate(item.ruta) },
                     icon = {
-                        Icon(imageVector=item.icon,
-                            contentDescription = item.title)
-                }
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = item.title
+                        )
+                    },
+                    label = {
+                        Text(text = item.title)
+                    }
                 )
             }
         }
